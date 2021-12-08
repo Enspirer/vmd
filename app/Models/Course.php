d5e49d37e6458c9ccfa0d71629352bdb72b45b2b
sanjayaharshana <sanjayaharasana@gmail.com>
2021-12-08 17:52:56 +0530

    Stype

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use App\Models\Auth\User;
-use App\Models\stripe\UserCourses;
+use App\Models\Stripe\UserCourses;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
